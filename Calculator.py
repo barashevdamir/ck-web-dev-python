8985ff33d2592ba9071a900e1b6270b369b164ae
barashevdamir <barashevdamir@yandex.ru>
2022-11-16 22:57:05 +0300

    Написать классы, которые будут использованы как счета в банке. Каждый счет - в своей валюте. Соответственно, у каждого объекта счета должны быть атрибуты с суммой денег, хранящихся на нём, и название кошелька. Каждый класс счета должен в себе хранить коэффициент отношения стоимости своей валюты к базовой валюте. Нам понадобится один базовый класс BaseWallet, в котором будут реализованы общие для всех валютных счетов методы, и три класса для конкретных валютных счетов: RubleWallet, DollarWallet, EuroWallet. Будем считать коэффициентами отношения валют к базовой валюте: Рубль: 1 Доллар: 60 Евро: 70 Критерии выполнения задания Протокол взаимодействия объектов следующий: RubleWallet("Первый кошелек", 10), где "Первый кошелек" - это название кошелька, а 10 - сумма денег на нём. аналогичные конструкторы для других счетов RubbleWallet("X", 10) + 20 == RubbleWallet("X", 30) - при сложении с числом считаем, что это та же валюта. RubbleWallet("X", 10) += 20 - должен поддерживаться и такой синтаксис 20 + RubbleWallet("X", 10) == RubbleWallet("X", 30) - radd для чисел RubbleWallet("X", 20) + DollarWallet("D", 10) == RubbleWallet("X", 80) - конвертация валюты при сложении счетов. DollarWallet("D", 2) + RubbleWallet("X", 60) == DollarWallet("D", 3) - результат - в валюте первого слагаемого. DollarWallet("D", 2) += RubbleWallet("X", 60) - здесь тоже должен поддерживаться этот синтаксис. предыдущие 6 пунктов реализовать и для вычитания RubbleWallet("X", 10) * 20 == RubbleWallet("X", 200) - умножение на число RubbleWallet("X", 10) *= 20 - тоже с таким синтаксисом те же 2 пункта для деления 20 * RubbleWallet("X", 10) == RubbleWallet("X", 200) - умножение числа на кошелек DollarWallet("A", 15) == DollarWallet("B", 15): два объекта равны, если у них совпадает тип кошелька и сумма на счете. RubleWallet("X", 100).spend_all() - для любого типа кошелька релизовать функцию, которая обнуляет баланс, если он положительный DollarWallet("X", 1).to_base() == 60 - эта функция должна возвращать число денег в кошельке в базовой валюте print(DollarWallet("Q", 150)) - должна выводить строку: 'Dollar Wallet Q 150' (и аналогично Ruble и Euro для остальных кошельков) У каждого объекта должны быть доступны атрибуты: name - название кошелька amount - количество денег на счете exchange_rate - коэффициент стоимости валюты к базовой Проверить решение задания вы можете в контесте в задаче Классы 2. Кошельки.

---
+++ Calculator.py
@@ -0,0 +1,45 @@
+class Calculator:
+    last = None
+
+    def __init__(self):
+        self.result = []
+
+    def sum(self, a, b):
+        s = f'sum({a}, {b}) == {round(a + b,1)}'
+        self.result.insert(0,s)
+        Calculator.last = s
+        return (a + b)
+
+    def sub(self, a, b):
+        s = f'sub({a}, {b}) == {round(a - b,1)}'
+        self.result.insert(0,s)
+        Calculator.last = s
+        return (a - b)
+
+    def mul(self, a, b):
+        s = f'mul({a}, {b}) == {round(a * b,1)}'
+        self.result.insert(0,s)
+        Calculator.last = s
+        return (a * b)
+
+    def div(self, a, b, mod=False):
+        if mod == True:
+            s = f'div({a}, {b}) == {round(a % b,1)}'
+            self.result.insert(0,s)
+            Calculator.last = s
+            return (a % b)
+        else:
+            s = f'div({a}, {b}) == {round(a / b,1)}'
+            self.result.insert(0,s)
+            Calculator.last = s
+            return (a / b)
+
+    def history(self, n):
+        if len(self.result) != 0:
+            return (self.result[n - 1])
+        else:
+            return None
+
+    @classmethod
+    def clear(cls):
+        cls.last = None
