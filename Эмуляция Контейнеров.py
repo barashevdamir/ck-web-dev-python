17698fd5b7444e07fe7b72663bf32f4398d3c840
barashevdamir <118205240+barashevdamir@users.noreply.github.com>
2022-11-21 22:14:11 +0300

    Update Эмуляция Контейнеров.py

--- Эмуляция Контейнеров.py
+++ Эмуляция Контейнеров.py
@@ -12,68 +12,39 @@ class Field(dict):
         """Функция, которая используется для текстового представления объекта в случаях, когда это происходит не
         через функцию str(obj) """
         return str(self)
-    def check_type_key(self, key):
-        self.key = key
-        if (type(self.key) != tuple or type(self.key != str)):
-            raise TypeError
-        else:
-            return True
-    def check_pattern_key(self, key):
-        self.key = key
-        if (re.findall(r'^[a-zA-Z]{1}\d{1,}$|^\d{1,}[a-zA-Z]{1}$', self.key)):
-            return True
-        else:
-            raise ValueError
-    # def check_key (self, key):
-    #     if (self.check_pattern_key(self, key) and self.check_type_key(self, key)):
-    #         if type (self.key) == str:
-    #             word = "".join(" " if el.isdigit() else el for el in key).split()
-    #             number = "".join(el if el.isdigit() else " " for el in key).split()
-    #             word = list(map(lambda x: x.lower(), word))
-    #             word = frozenset(word)
-    #             number = frozenset(number)
-    #             key_frozen = word.union(number)
-    #         else:
-    #             key_frozen = frozenset(key)
-    #         #    self.key = convert_tuple(key)
-    #         key_low = key.lower()
-    #         self.key = key_frozen
-    #         return self
-    def check_key(self, key):
-        self.key = key
-        if type(self.key) == tuple:
-            count = 0
-            self.key = list(self.key)
-            for i in self.key:
-                if type(i) == int:
-                    if i >= 0:
-                        self.key[count] = str(i)
-                    else:
-                        raise ValueError
-                elif type(i) == float:
+    def key_normalize(self, key):
+        comp_pat1 = re.compile(r'[a-zA-Z]\d+$')
+        comp_pat2 = re.compile(r'\d+[a-zA-Z]$')
+        if type(key) == tuple and len(key) == 2:
+            key = list(key)
+            for i in range(len(key)):
+                if type(key[i]) == int:
+                    key[i] = str(key[i])
+                elif type(key[i]) == str:
+                    pass
+                else:
                     raise ValueError
-                count += 1
-            self.key =''.join(self.key).upper()
-        elif type(self.key) == str:
-            self.key = self.key.upper()
+            key = ''.join(key)
+        if type(key) == str:
+            key = key.lower()
         else:
             raise TypeError
-        if re.match('\w\d+', self.key):
-            return self.key
-        elif re.match('\d+\w',self.key):
-            return re.search('[A-Z]+',self.key).group() + re.match('\d+', self.key).group()
+        if comp_pat1.match(key):
+            return key
+        if comp_pat2.match(key):
+            return key[::-1]
         else:
             raise ValueError
+    def __getitem__(self, key):
+        return super(Field, self).__getitem__(self.key_normalize(key))
     def __setitem__(self, key, value):
-        self.value = value
-        if self.check_key(key) == ValueError or self.check_key(key) == TypeError:
-            raise self.check_key(key)
-        super(Field, self).__setitem__(key, value)
-        dict.__setitem__(self, key, value)
-        #self.storage[self.normalize(key)] = self.value
-    def __delitem__(self, key, value):
-        super(Field, self).__delitem__(key)
+        super(Field, self).__setitem__(self.key_normalize(key), value)
+    def __delitem__(self, key):
+        super(Field, self).__delitem__(self.key_normalize(key))
     def __missing__(self, key):
-        return True
+        return None
     def __contains__(self, item):
         return self[item] != self.__missing__(1)
+    def __iter__(self):
+        for k, v in self.items():
+            yield v
