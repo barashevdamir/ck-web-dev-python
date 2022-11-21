d93602819e37c5847aece133339d67f244febab9
barashevdamir <barashevdamir@yandex.ru>
2022-11-21 08:26:00 +0300

    Напишем свой аналог листа таблицы Excel. Нужно написать структуру данных Field, в которой доступ к значениям будет осуществляться по ключам. Ключом будет пара "буква" - "число", по аналогии с адресом ячейки в Excel. Возможные форматы обращения к одной и той же "ячейке" данных: field = Field() field[1, 'a'] = 25 field['a', 1] = 25 field['a', '1'] = 25 field['1', 'a'] = 25 field['1a'] = 25 field['a1'] = 25 field[1, 'A'] = 25 field['A', 1] = 25 field['A', '1'] = 25 field['1', 'A'] = 25 field['1A'] = 25 field['A1'] = 25 В этом списке каждая из этих строк записывает число 25 в ячейку с одним и тем же ключом. Соответственно, по любому из перечисленных ключей должно быть можно получить это число из объекта field. Также должны быть реализованы удаление элемента из структуры (через оператор del) и возможность использования оператора in, например: (1, 'a') in field: True "A1" in field: True ('D', '4') in field: False Таким образом, выходит, что ключом структуры может быть либо кортеж, либо строка. При попытке получить или записать значение по ключу другого типа должно быть вызвано исключение TypeError. При некорректном значении строки или элементов кортежа нужно вызывать исключение ValueError. Корректными значениями будет считать одиночные буквы и неотрицательное целое число любой длины, т.е. правильные варианты ключей: А1 А222543 Z89 Неправильные варианты ключей: AA5 Q2.5 -6F A 27 GG Кроме вышеперчисленного, по объекту должно быть возможно итерироваться. При проходе циклом по объекту должны возвращаться значения, хранящиеся в нём. Порядок возврата значений не важен. Если запрашивается правильный формат ячейки, но в нашем контейнере такого ключа нет, то нужно вернуть None. Например: field = Field() print(field["C5"] is None) > True Примечания
    В своем решении этого задания я использовал в качестве ключей хранимого словаря frozenset, а проверку на ValueError реализовал через регулярку. Также рекомендую проверку типов и преобразование поступившего ключа в тот вид, в котором он хранится "под капотом", вынести в отдельный метод и вызывать его из всех описываемых магических методов.

--- Эмуляция Контейнеров.py
+++ Эмуляция Контейнеров.py
@@ -1,31 +1,46 @@
 import re
+
+def convert_tuple(c_tuple):
+  my_str=''
+  for i in c_tuple:
+    my_str=my_str+str(i)
+  return my_str
 class Field(dict):
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
+    def __init__(self):
+        super(Field, self).__init__()
     def __repr__(self):
         """Функция, которая используется для текстового представления объекта в случаях, когда это происходит не
         через функцию str(obj) """
         return str(self)
-
-    def check_pattern(self, key):
-        if (re.findall(r'^[a-zA-Z]{1}\d{1,}$|^\d{1,}[a-zA-Z]{1}$')):
-            return bool(key)
+    def check_type_key(self, key):
+        self.key = key
+        if (type(self.key) != tuple or type(self.key != str)):
+            return TypeError
+        else:
+            return True
+    def check_pattern_key(self, key):
+        self.key = key
+        if (re.findall(r'^[a-zA-Z]{1}\d{1,}$|^\d{1,}[a-zA-Z]{1}$', self.key)):
+            return True
         else:
             return ValueError
     def check_key (self, key):
-        if (type(key) != tuple or type(key != str)):
-            return TypeError
-        else:
-            if (self.check_pattern(key)):
-                if type (key) == tuple:
-                    key = ''.join(key)
-                key_low = key.lower()
-                key_frozen = frozenset(key_low)
-                return key_frozen
+        if (self.check_pattern_key(self, key) and self.check_type_key(self, key)):
+            if type (self.key) == str:
+                word = "".join(" " if el.isdigit() else el for el in key).split()
+                number = "".join(el if el.isdigit() else " " for el in key).split()
+                word = list(map(lambda x: x.lower(), word))
+                word = frozenset(word)
+                number = frozenset(number)
+                key_frozen = word.union(number)
+            else:
+                key_frozen = frozenset(key)
+            #    self.key = convert_tuple(key)
+            key_low = key.lower()
+            self.key = key_frozen
+            return self
     def __setitem__(self, key, value):
-        key_new = self.check_key(key)
-        super(Field, self).__setitem__(key_new, value)
+        super(Field, self).__setitem__(key, value)
     def __delitem__(self, key, value):
         super(Field, self).__delitem__(key)
     def __missing__(self, key, value):
